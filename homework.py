a2bd7118143318db04fc04a91c3193fcc7a0ed43
michaeljgrant <mjg.grant.m@gmail.com>
2024-06-27 21:49:03 +1000

    homework tasks for Tom

---
+++ homework.py
@@ -0,0 +1,50 @@
+# Python Homework 1
+# Complete these tasks and push them 
+# to github for review
+
+# Task 1: Print your name in the command line:
+
+
+
+# Task 2: Create 3 variables - one string - one integer - one boolean:
+
+
+
+# Task 3: Print your 3 variables to the command line:
+
+
+
+# Task 4: Create a function that accepts 1 argument & returns the argument
+
+
+
+
+# Task 5: Print the return of your function to the command line:
+# note: you must give the function 1 argument for it to print
+
+
+# Task 6: Ask the user what their name is in the command line:
+
+
+# Task 7: Print the "Your name is : " and the users name into the command line:
+
+
+
+# Task 8: Define a function that multiplies 2 numbers:
+
+
+
+
+# Task 9: Ask the user for 2 numbers (store them in variables)
+
+
+
+
+# Task 10: Use your multiplier function to calculate the result of the users 2 numbers
+# note: the numbers have not been converted to int() make sure to convert the users input
+
+
+
+# Task 11: Print the return of the multiplication to the command line:
+
+
