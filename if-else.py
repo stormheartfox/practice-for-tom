d19bcbee5efcefb1fe8070d723fbaa2175fc821e
michaeljgrant <mjg.grant.m@gmail.com>
2024-07-02 16:25:38 +1000

    add new branch for if-else logic

---
+++ if-else.py
@@ -0,0 +1,19 @@
+## Define if statement
+
+## if else
+
+## returns
+
+## elif
+
+# in a function
+
+def doesDiCaprioWantMyDaughter(ageOf):
+    if ageOf <= 25 and ageOf >= 18:
+        return "Leonardo would date your daughter!"
+    else:
+        return "Sorry Leonardo doesn't want her"
+
+print(doesDiCaprioWantMyDaughter(17))
+print(doesDiCaprioWantMyDaughter(26))
+print(doesDiCaprioWantMyDaughter(21))
