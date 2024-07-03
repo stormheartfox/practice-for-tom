c05c7f8d7caa8bd989b0fe2b87fb3936cb274b0b
michaeljgrant <mjg.grant.m@gmail.com>
2024-07-03 21:12:49 +1000

    Add homework 2

---
+++ Homework/homework2.py
@@ -0,0 +1,17 @@
+# Write a function that accepts 2 integers and an if statement that compares them
+# if they match - print they match, if they don't print that they don't match
+
+# Write a function that accepts a list and uses a for loop to print out all of the items in the list
+
+# write a for loop that prints out the indexes of your list in your function
+
+# Create a dictionary of cars and trucks, put 2 cars and 2 trucks in each 
+
+# create a list of every data type
+
+# write a while loop that counts to 10
+
+# write a function that accepts 2 numbers and multipies them
+
+# write a function that accepts 2 numbers and an operator and performs a math operation
+# based on which operator is given to the function
