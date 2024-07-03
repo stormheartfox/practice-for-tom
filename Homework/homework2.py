4edc760cfd77a50ceddf9d4f66a6ef169bd71cdd
michaeljgrant <mjg.grant.m@gmail.com>
2024-07-03 21:14:36 +1000

    Add task numbers

--- Homework/homework2.py
+++ Homework/homework2.py
@@ -1,17 +1,27 @@
+# Homework 2 - writing functions
+
+# Task 1
 # Write a function that accepts 2 integers and an if statement that compares them
 # if they match - print they match, if they don't print that they don't match
 
+# Task 2
 # Write a function that accepts a list and uses a for loop to print out all of the items in the list
 
+
+# Task 3
 # write a for loop that prints out the indexes of your list in your function
 
+# Task 4
 # Create a dictionary of cars and trucks, put 2 cars and 2 trucks in each 
 
+# Task 5
 # create a list of every data type
 
+
+# Task 6
 # write a while loop that counts to 10
 
-# write a function that accepts 2 numbers and multipies them
 
+# Task 7
 # write a function that accepts 2 numbers and an operator and performs a math operation
 # based on which operator is given to the function
