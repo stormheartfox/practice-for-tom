ace4bff962b1d12202a2127e29ea3fffa7d56701
michaeljgrant <mjg.grant.m@gmail.com>
2024-07-03 20:53:57 +1000

    add basic data types

---
+++ Datatypes/basic-datatypes.py
@@ -0,0 +1,21 @@
+# Within python we have a series of data types that are common across most of
+# the programming languages we have today
+# they are as follows
+
+# any numerical value without a decimal is called an integer
+integerDataType= 1 
+# any numerical value containing decimal points is called a float
+floatDataType= 1.5 
+# any characters contained within quotations is a string
+stringDataType= "words like this and even numbers like 10"  
+# the key words True and False without any quotations are the data type Boolean
+booleanTrue= True
+booleanFalse= False
+# There are also None(in other languages Null) types which are basically the absence of a value
+noneType= None
+# sometimes "" is also considered null or none but is not truly of the type
+
+# Other data types include lists & dictionaries, discussed in another file and 
+# other quite complex data types that have very specific uses.
+# The ones listed here are the most commonly seen day to day
+
