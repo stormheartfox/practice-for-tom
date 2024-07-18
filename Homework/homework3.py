4a1af1183dd00febf46b5797e1cbdc286a6f82a8
michaeljgrant <mjg.grant.m@gmail.com>
2024-07-18 22:06:40 +1000

    Add homework task 3

---
+++ Homework/homework3.py
@@ -0,0 +1,13 @@
+# Task 1
+# Take in strings of numbers such as "three", "fifty" etc. convert to numbers
+# Return value should be integers
+
+# Task 2
+# Create a program that plays scissors paper rock against the user
+# Make the choice of the computer player random
+# Consider how a CPU player would pick and how to select the winner
+# Hint: In-built modules in python can help you do this task
+
+# Task 3
+# In a new repo, start a python environment - install cowsay
+# take in user input and return cowsay of the user input
